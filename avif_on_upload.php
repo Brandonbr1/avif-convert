8922464b23b4b9ffdef6c13bc1e2c385ac26c93d
Java sauce <mohammedbrandon204@gmail.com>
2024-04-06 17:36:59 -0800

    Fix description

--- avif_on_upload.php
+++ avif_on_upload.php
@@ -2,8 +2,8 @@
 /**
  * Plugin Name: AVIF image on Upload
  * Plugin URI:  https://github.com/Brandonbr1/avif-convert
- * Description: A Wordpress plugin which converts files into .avif for optimal compression
- * Version:     1.0.3
+ * Description: A Wordpress plugin which converts files into .avif for optimal compression anf file size
+ * Version:     1.0.0
  * Author:      Hendy Tan, Brandonbr1 and Wordpress Authors
  * Author URI:  https://github.com/icaru12 , https://github.com/Brandonbr1
  * Text Domain: avif-on-upload
