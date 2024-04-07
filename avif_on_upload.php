a32693a0d41697e2eda09f601d513ed40ebae5a9
Java sauce <mohammedbrandon204@gmail.com>
2024-04-06 17:37:22 -0800

    fix info within file

--- avif_on_upload.php
+++ avif_on_upload.php
@@ -3,7 +3,7 @@
  * Plugin Name: AVIF image on Upload
  * Plugin URI:  https://github.com/Brandonbr1/avif-convert
  * Description: A Wordpress plugin which converts files into .webp for optimal compression and small file size.
- * Version:     1.0.3
+ * Version:     1.0.0
  * Author:      Hendy Tan, Brandonbr1 and Wordpress Authors
  * Author URI:  https://github.com/icaru12 , https://github.com/Brandonbr1
  * Text Domain: avif-on-upload
